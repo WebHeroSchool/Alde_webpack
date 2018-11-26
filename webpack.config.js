27013c03c693b624bb46799039188e81df9e91cd
Aleksey Denisov <alde.owl@gmail.com>
2018-11-26 23:10:16 +0700

    -

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const path = require('path');
 module.exports = {
   entry: './script.js',
   output: {
-    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.min.js'
   }
